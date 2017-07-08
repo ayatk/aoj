6d53d8c8e92fe3ae00507618fe7e1bf2434e0ed3
Aya Tokikaze <git@ayatk.com>
2017-07-09 05:42:10 +0900

    solve 1147

---
+++ vol11/1147.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import math
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+
+    s = []
+    for i in range(n):
+        s.append(int(input()))
+
+    s = sorted(s)
+    del s[0], s[-1]
+    print(math.trunc(sum(s)/len(s)))
